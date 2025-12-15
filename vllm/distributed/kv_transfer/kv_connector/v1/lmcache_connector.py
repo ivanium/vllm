62170cd0669b1c43b1423b81204ef148d830f9a9
Yifan Qiao <yifanqiao@berkeley.edu>
2025-12-14 17:19:30 -0800

    fixes
    Signed-off-by: Yifan Qiao <yifanqiao@berkeley.edu>

--- vllm/distributed/kv_transfer/kv_connector/v1/lmcache_connector.py
+++ vllm/distributed/kv_transfer/kv_connector/v1/lmcache_connector.py
@@ -27,44 +27,6 @@ if TYPE_CHECKING:
 logger = init_logger(__name__)
 
 
-class LMCacheKVEvents(KVConnectorKVEvents):
-    """
-    Concrete implementation of KVConnectorKVEvents using KVEventAggregator.
-    """
-
-    def __init__(self, num_workers: int) -> None:
-        self._aggregator = KVEventAggregator(num_workers)
-
-    def add_events(self, events: list[KVCacheEvent]) -> None:
-        self._aggregator.add_events(events)
-
-    def aggregate(self) -> "LMCacheKVEvents":
-        """
-        Aggregate KV events and retain only common events.
-        """
-        common_events = self._aggregator.get_common_events()
-        self._aggregator.clear_events()
-        self._aggregator.add_events(common_events)
-        self._aggregator.reset_workers()
-        return self
-
-    def increment_workers(self, count: int = 1) -> None:
-        self._aggregator.increment_workers(count)
-
-    def get_all_events(self) -> list[KVCacheEvent]:
-        return self._aggregator.get_all_events()
-
-    def get_number_of_workers(self) -> int:
-        return self._aggregator.get_number_of_workers()
-
-    def clear_events(self) -> None:
-        self._aggregator.clear_events()
-        self._aggregator.reset_workers()
-
-    def __repr__(self) -> str:
-        return f"<LMCacheKVEvents events={self.get_all_events()}>"
-
-
 class LMCacheConnectorV1(KVConnectorBase_V1, SupportsHMA):
     def __init__(
         self,
@@ -270,17 +232,3 @@ class LMCacheConnectorV1(KVConnectorBase_V1, SupportsHMA):
         # either list[int] or tuple[list[int], ...]. This could be changed in
         # the future to separate these two methods.
         return self._lmcache_engine.request_finished(request, block_ids)
-
-    def take_events(self) -> Iterable["KVCacheEvent"]:
-        """
-        Take the KV cache events from the connector.
-
-        Yields:
-            New KV cache events since the last call.
-        """
-        if self._kv_cache_events is not None:
-            self._kv_cache_events.aggregate()
-            kv_cache_events = self._kv_cache_events.get_all_events()
-            yield from kv_cache_events
-            self._kv_cache_events.clear_events()
-            self._kv_cache_events = None
